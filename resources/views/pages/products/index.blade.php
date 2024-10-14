5df348de09204510235ec995c9d8f1e662abf820
FigoArbiansyah <figoarbnsyh@gmail.com>
2024-10-14 14:20:46 +0700

    create, update, and delete product

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -18,17 +18,23 @@
     <div class="row">
         <div class="col">
             <div class="card">
+                <div class="card-header d-flex justify-content-end">
+                    <a href="/products/create" class="btn btn-sm btn-primary">
+                        Tambah Produk
+                    </a>
+                </div>
                 <div class="card-body">
                     <table class="table table-bordered">
                         <thead>
                             <tr>
                                 <th>No</th>
                                 <th>Produk</th>
-                                <th>Description</th>
+                                <th>Deskripsi</th>
                                 <th>Kode</th>
                                 <th>Harga</th>
                                 <th>Stok</th>
                                 <th>Kategori</th>
+                                <th>#</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -36,11 +42,25 @@
                                 <tr>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $product->name }}</td>
-                                    <td>{{ $product->description }}</td>
+                                    <td>{{ $product->description ?? '-' }}</td>
                                     <td>{{ $product->sku }}</td>
                                     <td>{{ $product->price }}</td>
                                     <td>{{ $product->stock }}</td>
                                     <td>{{ $product->category->name }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="/products/edit/{{ $product->id }}" class="btn btn-sm btn-warning mr-2">
+                                                Ubah
+                                            </a>
+                                            <form action="/products/{{ $product->id }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">
+                                                    Hapus
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                         </tbody>
